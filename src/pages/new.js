6d110edc9ae2055078a0597f9c4e0f591cdbf4ea
Ryan-TheLion <dae7249@nate.com>
2023-02-10 18:34:07 +0900

    fix: readItem API import 경로 수정
    ../api/request => ../api/movieRequest

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,33 +1,31 @@
-import {readItem} from '../api/request'
+import { readItem } from "../api/movieRequest";
 
-const newPage = document.createElement('div');
+const newPage = document.createElement("div");
 export default newPage;
 
-
-;(async ()=>{
-  const items = await readItem()
-  renderItems(items)
-})()
+(async () => {
+  const items = await readItem();
+  renderItems(items);
+})();
 
 // render
-function renderItems(items){
-  const liEls = items.filter(item =>item.tags.includes('신작'))
+function renderItems(items) {
+  const liEls = items.filter((item) => item.tags.includes("신작"));
 
-  const newItems = liEls.map(liEl =>{
+  const newItems = liEls.map((liEl) => {
     console.log(liEl);
     //div
-    const contentDivEl = document.createElement('div')
-    contentDivEl.classList.add('content__new')
+    const contentDivEl = document.createElement("div");
+    contentDivEl.classList.add("content__new");
 
     // img
-    const newImgEl = document.createElement('img')
-    newImgEl.src = liEl.thumbnail
+    const newImgEl = document.createElement("img");
+    newImgEl.src = liEl.thumbnail;
 
-    contentDivEl.append(newImgEl)
+    contentDivEl.append(newImgEl);
 
-    return contentDivEl
-  })
-  newPage.innerHTML = ''
-  newPage.append(...newItems)
+    return contentDivEl;
+  });
+  newPage.innerHTML = "";
+  newPage.append(...newItems);
 }
-
