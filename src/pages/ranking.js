87172201c6b09977252f011830fa6c4e377410ce
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-02-15 14:06:52 +0900

    feat: Implement ranking page

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,5 +1,69 @@
-const rankingPage = document.createElement('div');
-rankingPage.className = 'ranking-page';
-rankingPage.textContent = '랭킹 페이지';
+import { readItem } from '../api/movieRequest'
+import trailerList from '../trailer/trailerList'
 
-export default rankingPage;
+console.log(trailerList)
+
+const rankingFragment = document.createDocumentFragment()
+export default rankingFragment
+;(async () => {
+  const items = await readItem()
+  renderItems(items)
+})()
+
+const rankingImages = document.createElement('section')
+rankingImages.className = 'ranking-images'
+rankingImages.innerHTML = /* html */ `
+  <div class="inner">
+    <ol class="images-list">
+    </ol>
+  </div>
+`
+
+const rankingVideos = document.createElement('section')
+rankingVideos.className = 'ranking-videos'
+rankingVideos.innerHTML = /* html */ `
+  <div class="inner">
+    <ol class="videos-list"></ol>
+  </div>
+`
+
+rankingFragment.append(rankingImages)
+rankingFragment.append(rankingVideos)
+
+function renderItems(items) {
+  const imagesList = document.querySelector('.images-list')
+  const videosList = document.querySelector('.videos-list')
+
+  const imagesItems = items
+    .map((item, index) => {
+      index++
+      if (index > 10) return
+      return /* html */ `
+      <li class="images-item">
+        <a href="">
+          <img src=${item.thumbnail} />
+          <strong>${index}</strong>
+        </a>
+      </li>
+    `
+    })
+    .join('')
+
+  const videosItems = items
+    .map((item, index) => {
+      index++
+      console.log(item)
+      if (index > 10) return
+      return /* html */ `
+        <li class="videos-item">
+          <video src=${trailerList.get(item.title)} poster=${
+        item.thumbnail
+      } controls></video>
+        </li>
+    `
+    })
+    .join('')
+
+  imagesList.innerHTML = imagesItems
+  videosList.innerHTML = videosItems
+}
