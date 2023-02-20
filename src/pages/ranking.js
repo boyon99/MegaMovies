bf7dda8598454f6254942926d112acd1ce53ed30
boyon99 <bo_yon_99@naver.com>
2023-02-20 17:08:13 +0900

    fix: Reset file
    파일 원래 상태로 되돌림

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,7 +1,5 @@
 import { readItem } from '../api/movieRequest'
 import trailerList from '../trailer/trailerList'
-import imageList from '../trailer/imageList'
-
 
 const rankingContainer = document.createElement('div')
 export default rankingContainer
@@ -54,7 +52,9 @@ function renderItems(items) {
       if (index > 10) return
       return /* html */ `
         <li class="videos-item">
-          <video src=${trailerList.get(item.title)} poster=${imageList.get(item.title)} controls></video>
+          <video src=${trailerList.get(item.title)} poster=${
+        item.thumbnail
+      } controls></video>
         </li>
         `
     })
