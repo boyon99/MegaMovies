2ac5ba97700786805df94813d7ec6ae607bfb955
Ryan-TheLion <dae7249@nate.com>
2023-02-22 17:28:11 +0900

    feat: 거래 api 추가
    - 제품 거래 신청
    - 제품 거래 확정
    - 제품 거래 취소

---
+++ src/api/payment.js
@@ -0,0 +1,111 @@
+export default class paymentAPI {
+  #baseAPIURL = "https://asia-northeast3-heropy-api.cloudfunctions.net/api";
+  #defaultHeader = {
+    "Content-type": "application/json",
+    apikey: process.env.apikey,
+    username: "KDT4_Team1",
+  };
+
+  constructor() {
+    this.requestData = {
+      defaultData: {
+        baseUrl: this.#baseAPIURL,
+        headers: {
+          ...this.#defaultHeader,
+        },
+      },
+      order({ accessToken }) {
+        return {
+          url: `${this.defaultData.baseUrl}/products/buy`,
+          method: "POST",
+          headers: {
+            ...this.defaultData.headers,
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+      },
+      confirmOrder({ accessToken }) {
+        return {
+          url: `${this.defaultData.baseUrl}/products/ok`,
+          method: "POST",
+          headers: {
+            ...this.defaultData.headers,
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+      },
+      cancelOrder({ accessToken }) {
+        return {
+          url: `${this.defaultData.baseUrl}/products/cancel`,
+          method: "POST",
+          headers: {
+            ...this.defaultData.headers,
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+      },
+    };
+  }
+
+  // 제품 거래 신청
+  async order({ accessToken, productId, accountId }) {
+    if (!accessToken) return false;
+
+    const { url, method, headers } = this.requestData.order({
+      accessToken,
+    });
+
+    const response = await fetch(url, {
+      headers,
+      method,
+      body: JSON.stringify({
+        productId,
+        accountId,
+      }),
+    });
+
+    if (response.status !== 200) return false;
+
+    return true;
+  }
+
+  // 제품 거래 확정
+  async confirmOrder({ accessToken, productId }) {
+    if (!accessToken) return false;
+
+    const { url, method, headers } = this.requestData.confirmOrder({
+      accessToken,
+    });
+
+    const response = await fetch(url, {
+      headers,
+      method,
+      body: JSON.stringify({ detailId: productId }),
+    });
+
+    if (response.status !== 200) return false;
+
+    return true;
+  }
+
+  // 제품 거래 취소
+  async cancelOrder({ accessToken, productId }) {
+    if (!accessToken) return false;
+
+    const { url, method, headers } = this.requestData.cancelOrder({
+      accessToken,
+    });
+
+    const response = await fetch(url, {
+      headers,
+      method,
+      body: JSON.stringify({
+        detailId: productId,
+      }),
+    });
+
+    if (response.status !== 200) return false;
+
+    return true;
+  }
+}
