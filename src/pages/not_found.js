f4aa234163d59b923097ea2d95d38df37ebd0ef9
Myugaa <106896098+Myugaa@users.noreply.github.com>
2023-02-25 15:41:53 +0900

    Feat: Activate 404-not-found-page Countdown

--- src/pages/not_found.js
+++ src/pages/not_found.js
@@ -1,3 +1,5 @@
+import { router } from "..";
+
 export const notFoundPage = document.createElement("div");
 notFoundPage.classList.add("notFoundPage");
 
@@ -14,6 +16,23 @@ const NFsign = document.createElement("div");
 NFsign.innerText = "요청하신 페이지를 찾을 수 없습니다.";
 NFmain.appendChild(NFsign);
 
-const countdown = document.createElement("div");
-countdown.innerHTML = `<div>초 뒤, 홈 화면으로 자동 이동합니다.</div>`;
-NFmain.appendChild(countdown);
+let setTimeLimit = 5000;
+let secondsLeft = Date.now();
+
+const NFtimer = setInterval(() => {
+  const currentTime = Date.now();
+  const diff = currentTime - secondsLeft;
+  const remainingTime = Math.ceil((setTimeLimit - diff) / 1000);
+
+  let NFalert = `${remainingTime}초 뒤 홈 화면으로 자동 이동합니다.`;
+  const NFnotice = document.createElement("div");
+  NFnotice.classList.add("NFalert");
+  NFmain.appendChild(NFnotice);
+
+  if (remainingTime <= 0) {
+    clearInterval(NFtimer);
+    NFalert = "이동 중...";
+    router.navigate("");
+  }
+  document.querySelector(".NFalert").innerHTML = NFalert;
+}, 1000);
